ddc8aaa65e269f62ae6fb55048bef5ce19ace07e
Ferenc Hammerl <ferenchammerl@outlook.com>
2014-10-16 13:44:38 +0200

    config file edited

--- php/login.php
+++ php/login.php
@@ -10,20 +10,28 @@ if (!empty($_POST))
         else if (empty($_POST['password'])) {
             die("Please enter a password.");
         }
-        
-        $query = "SELECT 1 FROM users WHERE USERNAME = :username AND PASSWORD = :password";
-    
-        $query_params = array(':username' => $_POST['username'], ':password' => $_POST['password'] );
+
+        $query = "SELECT 1 FROM users WHERE USERNAME = :username";
+
+        $query_params = array(':username' => $_POST['username'] );
         try 
         {
             $stmt = $db->prepare($query);
             $result = $stmt->execute($query_params);
-        } 
+        }
+
+
         catch (PDOException $ex) 
         {
             die("Failed to run query: " . $ex->getMessage());
         }
-    
+
+        while($row = mysqli_fetch_array($result)) {
+            echo $row['username'] . " " . $row['password']. " " . $row['salt'];
+            echo "<br>";
+        }
+
+
         $row = $stmt->fetch();
         if ($row) {
             echo("WOHOO");
